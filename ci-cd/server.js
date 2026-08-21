365051145069a9485c2196d2d0a227944d3dacd9
GoutamChoudharyDev <rc.goutam.choudhary@gmail.com>
2026-08-21 11:20:51 +0530

    Create project

---
+++ ci-cd/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3004;
+
+app.get("/", (req, res) => {
+  res.send("Kubernetes Storage App is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
